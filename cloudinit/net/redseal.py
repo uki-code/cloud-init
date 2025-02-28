b28dd562b418bc5cb5be390302fdefbf1e08d63c
Alex Luehm <alex.luehm@ultimateknowledge.com>
2025-02-28 16:31:55 -0600

    Update IFACE_ROLE

--- cloudinit/net/redseal.py
+++ cloudinit/net/redseal.py
@@ -173,7 +173,7 @@ class Renderer(renderer.Renderer):
 
             # TODO: Move this to a redseal-specific module
             # set interface as an admin interface to support SSH
-            self.config_obj[f"IFACE_ROLE_{device_name}"] = "server-admin"
+            self.config_obj[f"IFACE_ROLE_{device_name}"] = "server-admin,data-collection"
 
             for subnet in interface.get("subnets", []):
 
